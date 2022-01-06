72793106c6daaa07ac49ce819acef057bd57ec8c
Seokhyun Bae <bus1029@gmail.com>
2022-01-06 22:10:59 +0900

    feat: 프록시 패턴과 데코레이터 패턴 실습을 위한 예제 V1 생성
    - 인터페이스와 구현 클래스가 있고 이를 스프링 빈으로 수동 등록하는 예제 생성
    - 이번 예제는 기존 프로젝트 구조를 유지한채로 진행하기 위해 proxy라는 패키지를 따로
    생성했으며 내부에 app 및 config 패키지를 생성한다
    - 그리고 ComponentScan 대상의 범위를 proxy/app 내부로 좁혀서 그 외의 외부 클래스들이
    컴포넌트 스캔의 대상이 되지 않도록 설정한다.

--- src/main/kotlin/hello/springadvanced/SpringAdvancedApplication.kt
+++ src/main/kotlin/hello/springadvanced/SpringAdvancedApplication.kt
@@ -1,9 +1,12 @@
 package hello.springadvanced
 
+import hello.springadvanced.proxy.config.AppV1Config
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
-@SpringBootApplication
+@Import(AppV1Config::class)
+@SpringBootApplication(scanBasePackages = ["hello.springadvanced.proxy"]) // 주의
 class SpringAdvancedApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/hello/springadvanced/proxy/app/v1/OrderRepositoryV1Impl.kt
@@ -0,0 +1,14 @@
+package hello.springadvanced.proxy.app.v1
+
+class OrderRepositoryV1Impl : OrderRepositoryV1 {
+  override fun save(itemId: String) {
+    if (itemId == "ex") {
+      throw IllegalStateException("예외 발생!")
+    }
+    sleep(1000)
+  }
+
+  private fun sleep(millis: Long) {
+    Thread.sleep(millis)
+  }
+}
