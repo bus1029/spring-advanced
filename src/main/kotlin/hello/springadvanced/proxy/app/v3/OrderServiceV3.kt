f56aca22f26421b93bc2f70d5bb0ad40bd580386
Seokhyun Bae <bus1029@gmail.com>
2022-01-17 20:52:40 +0900

    fix: OrderServiceV3의 잘못된 Repository 참조 수정
    - OrderServiceV3가 OrderRepositoryV3를 참조하지 않고 V2를 참조하고 있던 부분 수정

--- src/main/kotlin/hello/springadvanced/proxy/app/v3/OrderServiceV3.kt
+++ src/main/kotlin/hello/springadvanced/proxy/app/v3/OrderServiceV3.kt
@@ -1,11 +1,10 @@
 package hello.springadvanced.proxy.app.v3
 
-import hello.springadvanced.proxy.app.v2.OrderRepositoryV2
 import org.springframework.stereotype.Service
 
 @Service
 class OrderServiceV3 (
-  private val orderRepository: OrderRepositoryV2
+  private val orderRepository: OrderRepositoryV3
 ) {
   fun orderItem(itemId: String) {
     orderRepository.save(itemId)
