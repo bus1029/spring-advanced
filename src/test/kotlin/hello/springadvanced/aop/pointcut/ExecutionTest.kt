331f06378700ab27598c29ecb9e72ee164287dc3
Seokhyun Bae <bus1029@gmail.com>
2022-03-03 19:50:27 +0900

    feat: AspectJ 포인트컷 표현식 중 execution 학습 테스트 작성 2
    - 타입으로 매칭하는 케이스와 부모 타입의 메서드로 매칭하는 테스트
    케이스 추가
    - 파라미터 매칭 테스트 케이스 추가

--- src/test/kotlin/hello/springadvanced/aop/pointcut/ExecutionTest.kt
+++ src/test/kotlin/hello/springadvanced/aop/pointcut/ExecutionTest.kt
@@ -4,6 +4,7 @@ import hello.springadvanced.aop.member.MemberServiceImpl
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -90,6 +91,67 @@ class ExecutionTest {
   @Test
   fun packageMatchSubPackage2() {
     pointcut.expression = "execution(* hello.springadvanced.aop..*.*(..))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  fun typeExactMatch() {
+    pointcut.expression = "execution(* hello.springadvanced.aop.member.MemberServiceImpl.*(..))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  fun typeMatchSuperType() {
+    pointcut.expression = "execution(* hello.springadvanced.aop.member.MemberService.*(..))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  fun typeMatchNoSuperTypeMethodFalse() {
+    pointcut.expression = "execution(* hello.springadvanced.aop.member.MemberService.*(..))"
+    val internalMethod = MemberServiceImpl::class.java.getMethod("internal", String::class.java)
+    assertThat(pointcut.matches(internalMethod, MemberServiceImpl::class.java)).isFalse
+  }
+
+  @Test
+  fun typeMatchInternal() {
+    pointcut.expression = "execution(* hello.springadvanced.aop.member.MemberServiceImpl.*(..))"
+    val internalMethod = MemberServiceImpl::class.java.getMethod("internal", String::class.java)
+    assertThat(pointcut.matches(internalMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  @DisplayName("String 타입의 파라미터 허용")
+  fun argsMatch() {
+    pointcut.expression = "execution(* *(String))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  @DisplayName("파라미터가 없어야 함")
+  fun argsMatchNoArgs() {
+    pointcut.expression = "execution(* *())"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isFalse
+  }
+
+  @Test
+  @DisplayName("정확히 하나의 파라미터만 허용, 모든 타입 허용")
+  fun argsMatchStar() {
+    pointcut.expression = "execution(* *(*))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  @DisplayName("숫자와 무관하게 모든 파라미터, 모든 타입 허용")
+  fun argsMatchAll() {
+    pointcut.expression = "execution(* *(..))"
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
+  }
+
+  @Test
+  @DisplayName("String 타입으로 시작, 숫자와 무관하게 모든 파라미터, 모든 타입 허용")
+  fun argsMatchComplex() {
+    pointcut.expression = "execution(* *(String, ..))"
     assertThat(pointcut.matches(helloMethod, MemberServiceImpl::class.java)).isTrue
   }
 }
